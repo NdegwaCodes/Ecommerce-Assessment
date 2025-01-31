e80cda58daaa948e99ff7a58d4cc40c606ee06f3
Ndegwacodes_ <50831913+NdegwaCodes@users.noreply.github.com>
2025-01-31 05:19:08 +0300

    Add files via upload

---
+++ ecommerce-app-main/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiUrl: 'https://api.escuelajs.co/api',
+  stripePublishableKey:
+    'pk_test_51PqP6gFxB2dnnKdDhiB1cqZyvA0X1Si7ggeltnoO0OxXVRVMr35Lk8Z1UpsgbzjyawMNIpGlaB91S72uhbGvPpyJ00H5Zy8La7',
+};
